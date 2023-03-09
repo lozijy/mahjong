f09a191263f0e387bf9e9055b41ca4a0a96de90d
lozijy <92214888+lozijy@users.noreply.github.com>
2023-03-10 00:45:29 +0800

    添加gitignore

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,38 @@ import Vuex from "vuex"
 import Vue from "vue"
 Vue.use(Vuex);
 
+/* eslint-disable*/
 const mutations={
-    SELFDRAW: function(state,tile){
-        state.me.number++;
-        state.me.p_tile.unshift(tile);
+    get_me_id(state,player_id){
+        console.log("get_me_id");
+        state.me.id=player_id;
     },
-    // LEFTADDCARD: function(state,target){
-    //     state.target.number++;
-    // }
+    draw_self(state,tile){
+        state.me.number++;
+        state.me.p_tiles.unshift(tile);
+    },
+
+    draw_other(state,player_id){
+        console.log("draw_other");
+        const position = {
+            "-1" : "left",
+            "1" : "right",
+            "2" : "front",
+            "-2" : "front"
+        };
+        var str = player_id-state.me.player_id.toString();
+        state[position[str]].number++;
+    },
+
+    action_choose(state,action){
+        for(var i=0;i<action.length;i++){
+            state.options.unshift(action[i]);
+        }
+    },
+
+    countdown(state,time){
+        state.time=time;
+    },
 }
 
 const actions={
@@ -19,7 +43,8 @@ const actions={
 const state={
     me : {
         number:0,
-        p_tiles: []
+        p_tiles: [],
+        player_id:-1
         },
     front : {
         number:13,
