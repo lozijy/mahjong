94d62a545522c1f666d39a7300283772ada4897b
AOIINW <2826958322@qq.com>
2023-04-18 01:08:59 +0800

    notStarted的修改

--- src/views/game/components/notStarted.vue
+++ src/views/game/components/notStarted.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
     <div class="font">等待开始</div>
-    <div class="font">当前人数:{{$store.state.number+1}}</div>
+    <div class="font">当前牌桌人数:{{$store.state.number+1}}</div>
     <div class="font">玩家列表</div>
     <div class="font" v-for="person in $store.state.people" :key="person">{{ person.name }}</div>
-    <div  id="start" class="font" @click="start()">{{msg}}</div>
+    <div  id="start" class="font" @click="start()" >{{msg}}</div>
+    <!-- <div  id="start" class="font" @click="start()" :style="{ backgroundImage }"></div> -->
     <div id="countdown" class="font">准备倒计时:{{$store.state.time}}</div>
-    <div class="font" id="exit" @click="exit()">退出</div>
+    <div id="already"></div>
+    <div class="font_exit" id="exit" @click="exit()"></div>
   </div>
 </template>
 
@@ -21,9 +23,15 @@ export default {
   },
   methods:{
     start(){
-      this.msg="✓";
+      // this.msg = "√";
+      // this.$data.backgroundImage = '../../../../public/img/register.png';
+      // this.$set(this, 'backgroundImage', '../../../../public/img/register.png');
+      // this.backgroundImage = "../../../../public/img/register.png"; 
+      // console.log(document.getElementById("start"));
       this.exi.style.display = "none";
-      this.countDown.style.display="none";
+      this.countDown.style.display = "none";
+      this.sta.style.display = "none";
+      this.al.style.display="block"
       this.$root.$socket.send(JSON.stringify({
         "type":"ready"
       }));
@@ -48,7 +56,8 @@ export default {
   data(){
     return {
       // eslint-disable-next-line vue/no-dupe-keys
-      msg: "准备开始"
+      // backgroundImage: "../../../../public/img/ready1.png"
+      msg: ""
     }
   },
   computed:{
@@ -59,7 +68,8 @@ export default {
   mounted() {
     this.exi=document.getElementById("exit");
     this.sta=document.getElementById("start");
-    this.countDown=document.getElementById("countdown");
+    this.countDown = document.getElementById("countdown");
+    this.al = document.getElementById("already");
   },
   watch:{
     ready_flag: function(newValue, oldValue) {
@@ -86,32 +96,91 @@ export default {
 <style scoped>
 .container{
   display: block;
-  /* background-image: url("../public/img/table.png"); */
+  background-image: url("../../../../public/img/player_info1.png");
   background-color: #5f525260;
-  width: 20%;
+  background-position: contain;
+  background-size: contain;
+  background-repeat: no-repeat;
+  top:50%;
+  left:50%;
+  width: 42%;
   height: 60%;
   position: absolute;
-  top:30%;
-  left: 40%;
-  border: 70px;
+  transform: translate(-50%,-50%);
 }
 #start{
-  display: none;
-  border: 1px solid gray;
+  display: block;
+  /* border: 1px solid rgb(249, 247, 247); */
   cursor: pointer;
+  position: absolute;
+  background-image: url("../../../../public/img/ready1.png");
+  background-size:contain;
+  background-repeat: no-repeat;
+  width:22%;
+  height: 8%;
+  top:85%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  transition: transform 0.2s ease-in-out;
 }
+
+#start:hover{
+  transform: translate(-50%,-50%) scale(1.1);
+}
+
+#already{
+  display: none;
+  /* border: 1px solid rgb(249, 247, 247); */
+  /* cursor: pointer; */
+  position: absolute;
+  background-image: url("../../../../public/img/have_ready.png");
+  background-size:contain;
+  background-repeat: no-repeat;
+  width:22%;
+  height: 8%;
+  top:85%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  /* transition: transform 0.2s ease-in-out; */
+}
+
 #countdown{
   display: none;
 }
 #exit{
-  border: 1px solid gray;
+  /* border: 1px solid gray; */
   cursor: pointer;
+  display: block;
+  /* border: 1px solid rgb(249, 247, 247); */
+  position: absolute;
+  background-image: url("../../../../public/img/exit.png");
+  background-size:contain;
+  background-repeat: no-repeat;
+  width:22%;
+  height: 8%;
+  top:95%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  transition: transform 0.2s ease-in-out;
+}
+#exit:hover{
+  transform: translate(-50%,-50%) scale(1.1);
 }
 .font{
   font-size: 20px;
   position: relative;
-  top:50px;
+  top:10px;
   color: red;
 }
 
+.font_exit{
+  font-size: 20px;
+  position: absolute;
+  top:90%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  /* top:50px; */
+  color: red;
+
+}
 </style>
